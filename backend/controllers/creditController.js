388ae48a50ee0e6a38fd29b4f13e7199573d38e3
jpmartirez <johnpaulmartirez0915@gmail.com>
2025-12-27 00:38:04 +0800

    credit controller config

---
+++ backend/controllers/creditController.js
@@ -0,0 +1,62 @@
+import Transaction from "../models/Transaction";
+
+
+const plans = [
+    {
+        _id: "basic",
+        name: "Basic",
+        price: 10,
+        credits: 100,
+        features: ['100 text generations', '50 image generations', 'Standard support', 'Access to basic models']
+    },
+    {
+        _id: "pro",
+        name: "Pro",
+        price: 20,
+        credits: 500,
+        features: ['500 text generations', '200 image generations', 'Priority support', 'Access to pro models', 'Faster response time']
+    },
+    {
+        _id: "premium",
+        name: "Premium",
+        price: 30,
+        credits: 1000,
+        features: ['1000 text generations', '500 image generations', '24/7 VIP support', 'Access to premium models', 'Dedicated account manager']
+    }
+]
+
+// Get all subscription plans
+export const getPlans = async (req, res) => {
+    try {
+        res.json({success: true, plans});
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Controller for purchasing a plan
+export const purcahsePlan = async (req, res) => {
+    try {
+        const {planId} = req.body;
+
+        const userId = req.user._id;
+
+        const plan = plans.find(plan => plan._id === planId);
+
+        if (!plan){
+            return res.json({success: false, message: 'Invalid plan ID'});
+        }
+
+        const transaction = await Transaction.create({
+            userId,
+            planId: plan._id,
+            amount: plan.price,
+            credits: plan.credits,
+            isPaid: false,
+        })
+
+        
+    } catch (error) {
+        
+    }
+}
